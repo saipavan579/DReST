63d1303463632b888d21e3b79ae691a43d475a92
saipavan579 <saipavan579@gmail.com>
2015-02-09 09:59:28 +0530

    Create UploadBean.java

---
+++ UploadBean.java
@@ -0,0 +1,121 @@
+package com.Beans;
+
+import java.io.File;
+
+public class UploadBean {
+	
+	private String 	givenName;
+	private String 	surname;
+	private int 	age;
+	private char 	gender;
+	private String 	city;
+	private String 	district;
+	private String 	state;
+	private String 	country;
+	private String 	postalCode;
+	private boolean checkMe;
+	
+	private File userImage;
+	private String userImageContentType;
+	private String userImageFileName;
+	private String  patientID;
+	
+	public File getUserImage() {
+		return userImage;
+	}
+	public void setUserImage(File userImage) {
+		this.userImage = userImage;
+	}
+	
+	public String getUserImageContentType() {
+		return userImageContentType;
+	}
+	public void setUserImageContentType(String userImageContentType) {
+		this.userImageContentType = userImageContentType;
+	}
+	
+	public String getUserImageFileName() {
+		return userImageFileName;
+	}
+	public void setUserImageFileName(String userImageFileName) {
+		this.userImageFileName = userImageFileName;
+	}
+	 
+ 	public String getPatientID() {
+		return patientID;
+	}
+	public void setPatientID(String patientID) {
+		this.patientID = patientID;
+	}
+	public String getGivenName() {
+		return givenName;
+	}
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public boolean isCheckMe() {
+		return checkMe;
+	}
+	public void setCheckMe(boolean checkMe) {
+		this.checkMe = checkMe;
+	}
+
+
+}
